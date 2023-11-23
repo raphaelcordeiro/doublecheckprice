4135826200ce336db53e5bb466413423f1347e07
Raphael Cordeiro <raphael.cordeiro@justdigital.com.br>
2023-11-23 03:55:11 -0300

    Implement unit tests changes

--- Test/Unit/Plugin/ProductSaveBeforeTest.php
+++ Test/Unit/Plugin/ProductSaveBeforeTest.php
@@ -44,60 +44,10 @@ class ProductSaveBeforeTest extends TestCase
     public function testBeforeSaveWithPriceChange(): void
     {
         $this->markTestSkipped('This test is not yet implemented.');
-//        // Configuração do Mock de Produto
-//        $productMock = $this->createMock(Product::class);
-//        $productMock->method('getOrigData')->with('price')->willReturn(100);
-//        $productMock->method('getPrice')->willReturn(150);
-//
-//        // Configuração do Mock de DoubleCheckPriceInterface
-//        $this->doubleCheckPriceMock->method('setNewPrice')->with($this->equalTo(150));
-//
-//        // Assegurando que o mesmo objeto mockado seja retornado
-//        $this->doubleCheckPriceModelFactoryMock->method('create')->willReturn($this->doubleCheckPriceMock);
-//
-//        // Configuração das expectativas do método save
-//        $this->doubleCheckPriceRepositoryMock->expects($this->once())
-//            ->method('save')
-//            ->with($this->identicalTo($this->doubleCheckPriceMock));  // Uso de identicalTo
-//
-//        // Configuração adicional dos mocks
-//        $this->helperDataMock->method('isEmailNotificationEnabled')->willReturn(true);
-//        $userMock = $this->createMock(User::class);
-//        $userMock->method('getName')->willReturn('Test User');
-//        $this->helperDataMock->method('getUserByUsername')->willReturn($userMock);
-//
-//        $notificationMailDeliveryMock = $this->createMock(NotificationMailDelivery::class);
-//        $notificationMailDeliveryMock->expects($this->once())
-//            ->method('notificationMail');
-//
-//        $this->productSaveBefore->beforeSave($this->createMock(ProductRepository::class), $productMock);
     }
 
     public function testBeforeSaveNoPriceChangeButOtherAttributesChange(): void
     {
         $this->markTestSkipped('This test is not yet implemented.');
-//        $productMock = $this->createMock(Product::class);
-//        $productMock->method('getOrigData')->willReturnMap([
-//            ['price', 100],
-//            ['name', 'Original Name'],
-//            ['description', 'Original Description']
-//        ]);
-//        $productMock->method('getData')->willReturnMap([
-//            ['price', 100],
-//            ['name', 'New Name'],
-//            ['description', 'New Description']
-//        ]);
-//
-//        $this->doubleCheckPriceMock->expects($this->once())
-//            ->method('setNewPrice')
-//            ->with($this->equalTo(100));
-//
-//        $this->doubleCheckPriceRepositoryMock->expects($this->never())
-//            ->method('save');
-//
-//        $this->notificationMailDeliveryMock->expects($this->never())
-//            ->method('notificationMail');
-//
-//        $this->productSaveBefore->beforeSave($this->createMock(ProductRepository::class), $productMock);
     }
 }
